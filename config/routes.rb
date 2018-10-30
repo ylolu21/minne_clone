d7581847131ba5ca8a4413c91f5d3e42055bec6e
ylolu21 <ylolu21@gmail.com>
2018-10-30 21:18:08 +0900

    Modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'products#index'
-  get 'category' => 'categories#index'
-  resources :categories
   resources :products
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
