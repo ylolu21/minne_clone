eb63bc68759918132c160c449b0de07ab5d478ed
ylolu21 <ylolu21@gmail.com>
2018-11-20 21:03:58 +0900

    Devise install

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181030133522) do
+ActiveRecord::Schema.define(version: 20181114120401) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "category_code", null: false
@@ -35,6 +35,26 @@ ActiveRecord::Schema.define(version: 20181030133522) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_products_on_name"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "nickname", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "icon"
+    t.string "favorite_image"
+    t.string "homepage_url"
+    t.string "blog_url"
+    t.string "twitter_account"
+    t.string "facebook_account"
+    t.string "instagram_account"
+    t.string "mixi_account"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["nickname"], name: "index_users_on_nickname"
   end
 
 end
